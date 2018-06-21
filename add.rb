0c9296151432277d622139d7881b6a05c84b2c21
carolinebyron <byroncaroline2@gmail.com>
2018-06-21 15:41:33 +0000

    automatically backed up by learn

---
+++ add.rb
@@ -0,0 +1,10 @@
+practice = {
+  :grapes => "wine",
+  :clouds => "rain",
+  :orange => "juice" 
+}
+puts practice
+
+practice[:ice] = "water"
+puts "please, please, plesase make this work!!!!!!!!!!"
+puts practice
